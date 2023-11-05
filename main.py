8d53820bc50a1af12f52d0130d217f6ff9b7adfd
istbozhkov <ivan.bozhkov@mail.com>
2023-11-05 13:57:24 +0000

    encapsulated pricers into classes and moved to a separate module

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import numpy
-import logging
-from scipy.stats import norm
+import pricer_classes
 
-logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', filename='default.log', encoding='utf-8', level=logging.DEBUG)
+
 
 # Sample price history
 price_hist = [150, 153.07, 154.94, 155.86, 156.82, 153.3, 159.92,
@@ -47,195 +45,33 @@ apple_price_hist_2 = [177.970001, 179.070007, 175.490005, 173.929993, 174.789993
                       173.75, 172.40, 173.66, 175.01]
 
 
-def calc_volatility(stock_price_hist: list):
-    # A simple way to calculate volatility is to use  std deviation
-    # https://www.wallstreetmojo.com/volatility-formula/
-    # Note: there seems to be a slight error in their calc, because it's slightly
-    # different than the one calculated by numpy.std() - e.g.
-    # their mean is 162.23, although it's actually 162.5036
-    # This measure seems to give higher volatility than expected
-    logging.debug('Initiating calculation of volatility - method 1')
-    mean = numpy.mean(stock_price_hist)
-    daily_volat = numpy.std(stock_price_hist)
-    annualized_volat = numpy.sqrt(252)*daily_volat  # 252 is no. of trading days in a year
-    an_volat_percent = 100 * annualized_volat / mean
-    # Need to divide by the mean because annualized_volat is in absolute terms
-    logging.info(f'Calculated annualized volatility (1) = {annualized_volat}')
-    return an_volat_percent
-
-
-def calc_volatility_log(stock_price_hist: list):
-    # Using logarithmic returns
-    # Log returns are a way of measuring the percentage change over time
-    # https://www.macroption.com/historical-volatility-calculation/
-    # Compared to online volatility data, this method seems more accurate
-    logging.debug('Initiating calculation of volatility')
-    log_returns = [numpy.log(stock_price_hist[i] / stock_price_hist[i - 1])
-                   for i in range(1, len(stock_price_hist))]
-
-    # print(f"log_returns: {log_returns}")
-    daily_volat = numpy.std(log_returns)
-    # print(f"daily volatility: {daily_volat}")
-    annualized_volat = numpy.sqrt(252)*daily_volat  # 252 is no. of trading days in a year
-    an_volat_percent = 100 * annualized_volat
-    # No need to divide by the mean because annualized_volat is already
-    # calculated as a ratio from the log_returns formula
-    logging.info(f'Calculated annualized volatility (1) = {annualized_volat}')
-    return annualized_volat
-
-
-# print(calc_volatility(apple_price_hist_2))
-# print(calc_volatility_log(apple_price_hist_2))
-# print(calc_volatility_log(apple_price_hist))
-
-
-def calc_price_call(time_to_mat,underlying_price,strike_price,interest_rate,sigma):
-    logging.info(f'Initiating calculation of Call option price using B-S method, with the following parameters: \
-Time to maturity = {time_to_mat}, Underlying price = {underlying_price}, Strike price = {strike_price}, \
-Interest rate = {interest_rate}, Volatility = {sigma}')
-    N = norm.cdf    # Cumulative Distribution Function
-    d1 = ( 1 / (sigma*numpy.sqrt(time_to_mat)) ) * \
-        (numpy.log(underlying_price/strike_price) +
-        (interest_rate + (sigma**2)/2 )*time_to_mat)
-    d2 = d1 - sigma*numpy.sqrt(time_to_mat)
-    logging.debug(f'B-S calculated values: d1={d1}, d2={d2}')
-    # Black-Scholes formula explanation:
-    # Simplified:
-    # price_call =  Return - Cost
-    # price_call = (Stock Price * Probability Func) - (Probability Func X Strike Price X Discount to Present Value)
-    # for Probability Func we take into account volatility (sigma).
-    # Lower volatility would result in a narrower bell curve (higher certainty of price).
-    ret = N(d1) * underlying_price
-    cost = N(d2) * strike_price * numpy.exp(-interest_rate * time_to_mat)
-    price_call = ret - cost
-    logging.info(f'B-S calculated Call option price = {price_call}')
-    return price_call
-
-
-def calc_price_put(time_to_mat,underlying_price,strike_price,interest_rate,sigma):
-    logging.info(f'Initiating calculation of Put option price using B-S method, with the following parameters: \
-Time to maturity = {time_to_mat}, Underlying price = {underlying_price}, Strike price = {strike_price}, \
-Interest rate = {interest_rate}, Volatility = {sigma}')
-    N = norm.cdf    # Cumulative Distribution Function
-    d1 = ( 1 / (sigma*numpy.sqrt(time_to_mat)) ) * \
-         (numpy.log(underlying_price/strike_price) +
-          (interest_rate + (sigma**2)/2 )*time_to_mat)
-    d2 = d1 - sigma*numpy.sqrt(time_to_mat)
-    logging.debug(f'B-S calculated values: d1={d1}, d2={d2}')
-    # The put option formula is derived from the
-    # "put-call parity" expression, which is outside the scope of this project
-    # The resulting formula is:
-    price_put = N(-d2) * strike_price * numpy.exp(-interest_rate * time_to_mat)\
-                - N(-d1) * underlying_price
-    logging.info(f'B-S calculated Call option price = {price_put}')
-    return price_put
-
-
 # Sample values:
 t = 1     # in years
 S = 174.9   # Underlying price USD
 K = 190     # Strike price USD
 r = 0.05
-sig = calc_volatility_log(apple_price_hist_2)
+# sig = calc_volatility_log(apple_price_hist_2)
+
+apple_BS = pricer_classes.BlackScholes(time_to_mat=t, underlying_price=S, strike_price=K,
+                                       stock_price_hist=apple_price_hist, interest_rate=r)
 
 for K in range(140,191,5):
-
-    print(f"K={K}, call: S={calc_price_call(t,S,K,r,sig)}, put: S={calc_price_put(t,S,K,r,sig)}")
-
-
-# There's conflicting info on what Monte Carlo actually is.
-# From this https://www.wallstreetmojo.com/option-pricing-2/:
-# Monte Carlo is a method of calculating option prices by taking random
-# variables (unlike BS model that takes 5 give variables).
-# The Monte Carlo option model is the application of Monte Carlo Methods.
-# This pricing model uses random samples to calculate the price.
-# This method is more favorable than other methods like Black-Scholes
-# for calculating the value of options with multiple sources of uncertainty.
-# From wikipedia: Since the underlying random process is the same,
-# for enough price paths, the value of a european option here should be
-# the same as under Black–Scholes.
-# MC process:
-# 1. Simulate many stock price trajectories
-# 2. Average the stock price for each of the simulated paths.
-# 3. Calculate the payoff (option premium) for each simulated price.
-# 4. Average the payoffs for all paths.
-# http://www.goddardconsulting.ca/option-pricing-monte-carlo-index.html
-
-# Weiner process (Used by MC to simulate stock price evolution):
-# price_t   -> underlying price at time t
-# price_0 = 175   # underlying price now - in USD
-# mu = 0.05   # asset's historical return TODO: find a formula for this
-# volat = calc_volatility_log(apple_price_hist_2)     # expected volatility
-# dt = 1/252  # time step - in years
-# z = numpy.random.normal()   # a random number from a normal distribution
-# with a peak of 0, 1std distribution is +/- 1, etc.
-
-# price_t = price_0*numpy.exp(((mu-(volat**2)/2)*dt)+(volat*numpy.sqrt(dt)*z))
-
-
-def calc_mc(price_0, mu, volat, steps, time_to_mat):
-    # Weiner process (Used by MC to simulate stock price evolution):
-    # price_t   -> underlying price at time t
-    # price_0   -> starting underlying price - in USD
-    # mu     -> asset's historical return TODO: find a formula for this
-    # volat     -> expected volatility
-    # steps    -> time step - in years
-    # z     -> a random number from a normal distribution
-    # with a peak of 0, 1std distribution is +/- 1, etc.
-    # TODO: calculate only last value and move trajectory calc to another def (for creating a plot later)
-    logging.info(f'Initiating stock price simulation using the Monte Carlo model, with the following parameters: \
-Time to maturity = {time_to_mat}, Underlying price at start = {price_0}, \
-Asset\'s historical return = {mu}, Volatility = {volat}')
-    # pre-calculating all z values in the following array
-    # simple testing showed reduction of ~10% in processing time compared to
-    # calculating z value every iteration.
-    z_array = numpy.random.normal(size=steps)
-    dt = time_to_mat/steps
-    price_t = price_0*numpy.exp(((mu-(volat**2)/2)*dt)+(volat*numpy.sqrt(dt)*z_array[0]))
-    price_hist = [price_t]
-    for i in range(steps):
-        z = z_array[i]
-        price_t = price_t*numpy.exp(((mu-(volat**2)/2)*dt)+(volat*numpy.sqrt(dt)*z))
-        price_hist.append(price_t)
-    logging.info(f'Calculated stock price after {time_to_mat} years: {price_hist[-1]}')
-    return price_hist[-1]
-
+    apple_BS.strike_price = K
+    apple_BS.calc_price_call()
+    apple_BS.calc_price_put()
+    print(f"K={K}, call: S={apple_BS.price_call}, put: S={apple_BS.price_put}")
 
 # Sample values for MC:
 p0 = 175
 mu = 0.05
-sigma = calc_volatility_log(apple_price_hist_2)
 step = 252
 t = 1
+n=100
 
-# Call option payoff is calculated as: Spot price - Strike price
-# Put option payoff is calculated as: Strike price - Spot price
-# Payoff cannot be negative.
+apple_MC = pricer_classes.MonteCarlo(time_to_mat=t, underlying_price=p0, strike_price=180,
+                                     stock_price_hist=apple_price_hist, mu=mu)
 
+apple_MC.option_price_mc(n=n)
+print(f"MC calculated prices with {n} iterations: call = {apple_MC.call_price}, put = {apple_MC.put_price}")
 
-def option_price_mc(price_0, mu, volat, steps, time_to_mat, n: int, K):
-    """Calculate option price for given parameters
-    n - number of iterations for simulation to run
-    Simulate n trajectories and calculate payoff for each.
-    Return averaged payoff"""
-    logging.debug(f'Starting Option price calculation from MC simulated prices with {n} simulations')
-    # Initializing empty numpy arrays:
-    simulated_strike_prices = numpy.empty(n)
-    simulated_call_payoffs = numpy.empty(n)
-    simulated_put_payoffs = numpy.empty(n)
-
-    for i in range(n):
-        simulated_strike_price = calc_mc(price_0, mu, volat, steps, time_to_mat)
-        simulated_strike_prices[i] = simulated_strike_price
-        # Currently not used. Keeping for future use for plots
-        # print(simulated_strike_prices)
-        simulated_call_payoffs[i] = max(simulated_strike_price - K, 0)
-        simulated_put_payoffs[i] = max(K - simulated_strike_price, 0)
-
-    call_price = numpy.mean(simulated_call_payoffs)
-    put_price = numpy.mean(simulated_put_payoffs)
-    logging.info(f'Calculated option prices using MC method: Call = {call_price}, Put = {put_price}')
-    return call_price, put_price
-
-
-print(option_price_mc(p0, mu, sigma, step, t, 100, 180))
+# print(option_price_mc(p0, mu, sigma, step, t, 100, 180))
